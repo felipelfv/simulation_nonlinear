13d1b6fc4f22a868965cd38b8ae3272511cd6f7b
felipelfv <felipelfv98@gmail.com>
2025-06-25 14:45:18 +0200

    clean up and descriptions

--- Simulation_Scripts/Simulation_Study_1/Simulation_1/Models(1).R
+++ Simulation_Scripts/Simulation_Study_1/Simulation_1/Models(1).R
@@ -1,15 +1,22 @@
-#### 1. General Information ####
+##==============================================================================
+## General Information 
+##
+## Script name: Models(1).R
+##
+## Purpose of script: The models defined here (lavaan-syntax) are used to 
+## generate data (specified in GenerateData() through parameter `model`) as well 
+## as to fit/estimate through the functions defined in script Methods(1).R. For 
+## the former, we have the population.X.model(s), for the latter we have the 
+## fit.X.model(s).
+##
+##==============================================================================
 
-#################
-#### Study 1 ####
-#################
+##==============================================================================
+## 1. Population Models 
+##==============================================================================
 
-#### 2. Population Models - Under the Alternative ####
+##### 1.1 First Population Model (M_{lin}) #####
 
-# Here we start with the population models generated under the alternative
-# hypothesis 
-
-##### 2.1 First Population Model (M_{lin}) #####
 # No interaction terms and only used to examine the Type I error rate
 # \eta = 0.316*\xi_{1} + 0.316*\xi_{2} + \zeta
 
@@ -28,7 +35,9 @@ eta1 ~~ 1*eta1
 eta2 ~~ 1*eta2
 "
 
-##### 2.2 Second Population Model (M_{int}) #####
+##### 1.2 Second Population Model (M_{int}) #####
+
+# Note: this was not used in the simulation study in the end
 # One interaction included
 # \eta = -0.255 + 0.316*\xi_{1} + 0.316*\xi_{2} + 0.139*\xi_{1}\xi_{2} + \zeta
 
@@ -47,7 +56,8 @@ eta1 ~~ 1*eta1
 eta2 ~~ 1*eta2
 "
 
-##### 2.3 Third Population Model (M_{full}) #####
+##### 1.3 Third Population Model (M_{full}) #####
+
 # Two quadratic effects (on top of the interaction)
 # \eta = -0.255 + 0.316*\xi_{1} + 0.316*\xi_{2} + 0.139*\xi_{1}\xi_{2} + 
 # 0.101*\xi_{1}^2 + 0.101*\xi_{2}^2 + \zeta
@@ -67,9 +77,15 @@ eta1 ~~ 1*eta1
 eta2 ~~ 1*eta2
 "
 
-#### 3. Fitted Models ####
+##==============================================================================
+## 2. Fit Models 
+##==============================================================================
 
-##### 3.1 First Fitted Model (A_{int}) #####
+
+##### 2.1 First Fitted Model (A_{int}) #####
+
+# Note: this was not used in the simulation study in the end
+
 fit.interaction.model <- "
 # Measurement model
 eta1 =~ x1 + x2 + x3
@@ -85,7 +101,8 @@ eta1 ~~ eta1
 eta2 ~~ eta2
 "
 
-##### 3.2 Second Fitted Model (A_{full}) #####
+##### 2.2 Second Fitted Model (A_{full}) #####
+
 fit.full.model <- "
 # Measurement model
 eta1 =~ x1 + x2 + x3
